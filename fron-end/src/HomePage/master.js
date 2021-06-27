30e909925b7d7adf64c6b46aa23f02635a4981cc
unknown <hai.huynh22@student.passerellesnumeriques.org>
2021-06-27 14:23:14 +0700

    Add enrollment, Share

--- fron-end/src/HomePage/master.js
+++ fron-end/src/HomePage/master.js
@@ -1,59 +1,72 @@
-import React, { Component } from 'react';
-import Header from '../components/header';
-import Footer from '../components/footer';
+import React, { Component } from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import Share from "../components/Share";
+import Enrollment from "../components/Enrollment";
+
+
 class Master extends Component {
-    render() {
-        return (
-            <div>
-                <Header></Header>
-                {/* Hero End */}
-                {/* About Start */}
-                
-                {/* About End */}
-                {/* Service Start */}
-               
-                {/* Service End */}
-                {/* Experience Start */}
-              
-                {/* Job Experience End */}               
-                {/* Banner Start */}
-               
-                {/* Banner End */}              
-                {/* Portfolio Start */}
- 
-                {/* Portfolio End */}         
-                {/* Banner Start */}
-  
-                {/* Banner End */}
-                {/* Sesstion Start */}
-                
-               
-                {/* Testimonials End */}
-                {/* Testimonial Start */}
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        {/* Hero End */}
+        {/* About Start */}
 
-                {/* Testimonial End */}
-               
-                {/* Team Start */}
+        {/* About End */}
+        {/* Service Start */}
 
-                {/* Team End */}
-                {/* Contact Start */}
+        {/* Service End */}
+        {/* Experience Start */}
 
-                {/* Contact End */}
-                {/* Blog Start */}
+        {/* Job Experience End */}
+        {/* Banner Start */}
 
-                {/* Blog End */}
-                {/* Footer Start */}
-                <Footer></Footer>
-                {/* Footer End */}
-                {/* Back to top button */}
-                <a href="#" className="btn back-to-top"><i className="fa fa-chevron-up" /></a>
-                {/* Pre Loader */}
-                <div id="loader" className="show">
-                    <div className="loader" />
-                </div>
-            </div>
-        );
-    }
+        {/* Banner End */}
+
+        {/* Enrollment Start */}
+        <Enrollment></Enrollment>
+        {/* Enrollmet End */}
+
+        {/* Share start */}
+        <Share></Share>
+        {/* Share end */}
+
+        {/* Banner Start */}
+
+        {/* Banner End */}
+        {/* Sesstion Start */}
+
+        {/* Testimonials End */}
+        {/* Testimonial Start */}
+
+        {/* Testimonial End */}
+
+        {/* Team Start */}
+
+        {/* Team End */}
+        {/* Contact Start */}
+
+        {/* Contact End */}
+        {/* Blog Start */}
+
+        {/* Blog End */}
+
+        {/* Footer Start */}
+       
+        <Footer></Footer>
+        {/* Footer End */}
+        {/* Back to top button */}
+        <a href="#" className="btn back-to-top" style={{ marginRight: "9vh" }}>
+          <i className="fa fa-chevron-up" />
+        </a>
+        {/* Pre Loader */}
+        <div id="loader" className="show">
+          <div className="loader" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Master;
